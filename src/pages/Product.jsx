cc87b4507f2e390719e2bd252aaada862d4c3a90
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-01-18 15:25:20 +0530

    Products vercel build issue

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,17 +5,14 @@ import Marquee from "react-fast-marquee";
 import { useDispatch } from "react-redux";
 import { addCart } from "../redux/action";
 
-import { Footer, Navbar } from "../components";
-
 const Product = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
 
   const [product, setProduct] = useState({});
   const [similarProducts, setSimilarProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loading2, setLoading2] = useState(false);
-
-  const dispatch = useDispatch();
 
   const addProduct = (product) => {
     dispatch(addCart(product));
@@ -30,153 +27,76 @@ const Product = () => {
         setLoading(true);
         setLoading2(true);
 
-        const response = await fetch(
+        const res = await fetch(
           `https://fakestoreapi.com/products/${id}`,
           { signal }
         );
-        const data = await response.json();
+        const data = await res.json();
         setProduct(data);
         setLoading(false);
 
-        const response2 = await fetch(
+        const res2 = await fetch(
           `https://fakestoreapi.com/products/category/${data.category}`,
           { signal }
         );
-        const data2 = await response2.json();
+        const data2 = await res2.json();
         setSimilarProducts(data2);
         setLoading2(false);
-      } catch (error) {
-        if (error.name !== "AbortError") {
-          console.error(error);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.error(err);
         }
       }
     };
 
     getProduct();
-
     return () => controller.abort();
   }, [id]);
 
   const Loading = () => (
     <div className="container my-5 py-2">
-      <div className="row">
-        <div className="col-md-6 py-3">
-          <Skeleton height={400} width={400} />
-        </div>
-        <div className="col-md-6 py-5">
-          <Skeleton height={30} width={250} />
-          <Skeleton height={90} />
-          <Skeleton height={40} width={70} />
-          <Skeleton height={50} width={110} />
-          <Skeleton height={120} />
-          <Skeleton height={40} width={110} inline />
-          <Skeleton className="mx-3" height={40} width={110} />
-        </div>
-      </div>
+      <Skeleton height={400} />
     </div>
   );
 
   const ShowProduct = () => (
     <div className="container my-5 py-2">
       <div className="row">
-        <div className="col-md-6 col-sm-12 py-3">
-          <img
-            className="img-fluid"
-            src={product.image}
-            alt={product.title}
-            width="400"
-            height="400"
-          />
+        <div className="col-md-6">
+          <img src={product.image} alt={product.title} height="400" />
         </div>
-        <div className="col-md-6 py-5">
-          <h4 className="text-uppercase text-muted">{product.category}</h4>
-          <h1 className="display-5">{product.title}</h1>
-          <p className="lead">
-            {product.rating?.rate} <i className="fa fa-star"></i>
-          </p>
-          <h3 className="display-6 my-4">${product.price}</h3>
-          <p className="lead">{product.description}</p>
+        <div className="col-md-6">
+          <h1>{product.title}</h1>
+          <h3>${product.price}</h3>
+          <p>{product.description}</p>
           <button
-            className="btn btn-outline-dark"
+            className="btn btn-dark"
             onClick={() => addProduct(product)}
           >
             Add to Cart
           </button>
-          <Link to="/cart" className="btn btn-dark mx-3">
-            Go to Cart
-          </Link>
         </div>
-      </div>
-    </div>
-  );
-
-  const Loading2 = () => (
-    <div className="my-4 py-4">
-      <div className="d-flex">
-        {[1, 2, 3, 4].map((item) => (
-          <div key={item} className="mx-4">
-            <Skeleton height={400} width={250} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-
-  const ShowSimilarProduct = () => (
-    <div className="py-4 my-4">
-      <div className="d-flex">
-        {similarProducts.map((item) => (
-          <div key={item.id} className="card mx-4 text-center">
-            <img
-              className="card-img-top p-3"
-              src={item.image}
-              alt={item.title}
-              height={300}
-              width={300}
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                {item.title.substring(0, 15)}...
-              </h5>
-            </div>
-            <div className="card-body">
-              <Link
-                to={`/product/${item.id}`}
-                className="btn btn-dark m-1"
-              >
-                Buy Now
-              </Link>
-              <button
-                className="btn btn-dark m-1"
-                onClick={() => addProduct(item)}
-              >
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        ))}
       </div>
     </div>
   );
 
   return (
     <>
-      <Navbar />
-      <div className="container">
-        <div className="row">
-          {loading ? <Loading /> : <ShowProduct />}
-        </div>
+      {loading ? <Loading /> : <ShowProduct />}
 
-        <div className="row my-5 py-5">
-          <div className="d-none d-md-block">
-            <h2>You may also Like</h2>
-            <Marquee pauseOnHover pauseOnClick speed={50}>
-              {loading2 ? <Loading2 /> : <ShowSimilarProduct />}
-            </Marquee>
-          </div>
-        </div>
-      </div>
-      <Footer />
+      <h2 className="text-center my-4">You may also like</h2>
+      <Marquee pauseOnHover speed={50}>
+        {loading2
+          ? <Skeleton height={300} width={200} />
+          : similarProducts.map((item) => (
+              <div key={item.id} className="card mx-3">
+                <img src={item.image} alt={item.title} height={200} />
+                <Link to={`/product/${item.id}`} className="btn btn-dark">
+                  Buy Now
+                </Link>
+              </div>
+            ))}
+      </Marquee>
     </>
   );
 };
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Footer, Navbar, Product } from "../components"
+import React from "react";
+import { Footer, Navbar } from "../components";
+import Product from "../components/Product";
 
 const Products = () => {
   return (
@@ -8,7 +9,7 @@ const Products = () => {
       <Product />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
